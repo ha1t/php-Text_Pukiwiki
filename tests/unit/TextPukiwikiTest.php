fb0b08db66f7ce2c10967cc3199cc5e6dce3b5d5
ha1t <halt.feits@gmail.com>
2013-08-06 14:14:29 +0900

    add parseP test

--- tests/unit/TextPukiwikiTest.php
+++ tests/unit/TextPukiwikiTest.php
@@ -1,10 +1,12 @@
 <?php
 class Text_PukiwikiTest extends PHPUnit_Framework_TestCase
 {
-    public function testBasic()
+    public function testParseP()
     {
+        $test_text = 'hoge';
         $pukiwiki = new Text_Pukiwiki();
-        $result = $pukiwiki->toHtml('hoge');
+        $result = $pukiwiki->toHtml($test_text);
+        $this->assertEquals('<p>hoge</p>', $result);
         $this->assertGreaterThan(0, strlen($result));
     }
 }
